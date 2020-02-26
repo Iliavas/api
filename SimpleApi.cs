180cfa178377d81543151256cad5b8ed68b47d77
ilvsl <il.vsl0110@gmail.com>
2020-02-26 13:38:16 +0300

    ''

---
+++ SimpleApi.cs
@@ -0,0 +1,39 @@
+﻿namespace FilterAPi
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public enum Types_class : int {
+        Russian,
+        Engilsh
+    }
+    public struct cab
+    {
+        public Types_class type;
+        public int number;
+    }
+    public class MainClass
+    { 
+        public List<cab> cabs = new List<cab>();
+        public void add(int num, Types_class type) {
+            cab c;
+            c.type = type;
+            c.number = num;
+            cabs.Add(c); 
+        }
+
+        public List<cab> filter(Types_class type)
+        {
+            List<cab> a = new List<cab>();
+            foreach(cab i in cabs)
+            {
+                if (i.type == type)
+                {
+                    a.Add(i);
+                }
+            }
+            return a;
+        }
+    }
+}
